d59fc2172b18a93aa1f52be4dc7b83282152e89c
martinBernad <bernadmartin.mb@gmail.com>
2024-05-30 19:04:14 +0200

    new code

---
+++ 06_thermometer.py
@@ -0,0 +1,8 @@
+
+def temp_add():
+    temperatures = []
+    add = float("Do you want to add new temperature? (y/n) -> ")
+def temp_sort(): pass
+def temp_max(): pass
+def temp_min(): pass
+def control(): pass
